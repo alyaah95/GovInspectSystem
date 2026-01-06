c3aa78cd14873f43a45d15b5f81fe27d74080250
alyaah95 <alyaah883@gmail.com>
2026-01-07 01:07:55 +0200

    fix sending reset password email to user

--- inspectors/urls.py
+++ inspectors/urls.py
@@ -64,7 +64,7 @@ urlpatterns = [
     path('inspector/rejected-reports/', views.inspector_rejected_reports_view, name='inspector_rejected_reports'),
     
     # مسارات إعادة تعيين كلمة المرور
-    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='inspectors/password_reset_form.html',html_email_template_name='inspectors/password_reset_email.html',subject_template_name='inspectors/password_reset_subject.txt'), name='password_reset'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='inspectors/password_reset_form.html',html_email_template_name='inspectors/password_reset_email.html'), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='inspectors/password_reset_done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='inspectors/password_reset_confirm.html', form_class=InspectorSetPasswordForm), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='inspectors/password_reset_complete.html'), name='password_reset_complete'),
