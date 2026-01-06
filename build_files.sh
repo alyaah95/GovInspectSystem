71f4fe75e8dcb831c5bee7f46a264c7933b1451b
alyaah95 <alyaah883@gmail.com>
2026-01-06 02:54:45 +0200

    Fix: Update Vercel config and routes

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,5 @@
-# تثبيت المكتبات الموجودة في requirements.txt
-pip install -r requirements.txt
-
-# تجميع ملفات الـ CSS والـ JS (Static Files)
-python3.9 manage.py collectstatic --noinput
-
-# (اختياري) تنفيذ الميجريشن إذا كنتِ ربطتِ قاعدة البيانات بنجاح
-python3.9 manage.py migrate --noinput
+#!/bin/bash
+echo "Building the project..."
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
+echo "Build finished!"
