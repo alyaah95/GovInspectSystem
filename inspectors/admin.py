298448c9b9dbfa40284c592bbd0f7a488e042517
alyaah95 <alyaah883@gmail.com>
2026-01-06 22:55:02 +0200

    Fix: Update admin.py

--- inspectors/admin.py
+++ inspectors/admin.py
@@ -42,7 +42,7 @@ class UserAdmin(BaseUserAdmin):
                 ),
             },
         ),
-        (_("Important dates"), {"fields": ("last_login",)}),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
     )
 
     add_fieldsets = (
